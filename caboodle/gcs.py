f6c13704483c9db0426b048d0a4a6758a50b3bf7
Saad Khan <skhan8@mail.einstein.yu.edu>
2020-01-21 16:41:48 -0500

    fix import error

--- caboodle/gcs.py
+++ caboodle/gcs.py
@@ -4,6 +4,7 @@ from typing import List, Tuple, Union
 import io
 import warnings
 import os
+from tqdm import tqdm
 
 def printv(*args, verbose=True, **kwargs):
     if verbose:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Saad Khan'
 author = 'Saad Khan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
